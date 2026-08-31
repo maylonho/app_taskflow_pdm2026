11051ea202ce5f2bcbd03f4b75b32d0e99027db2
Maylon Henrique de Oliveira <moliveira@academico.intra>
2026-08-31 18:51:22 -0300

    Aula 05 - Componente Interativos

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,41 @@
-import { Image, Text, View } from 'react-native';
+import { Image, Pressable, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from './styles';
 
 export default function Home() {
+    function iniciarAplicacao() {
+        console.log("O botão foi pressionado!")
+    }
     return (
         <SafeAreaView style={styles.safeArea}>
             <View style={styles.container}>
-                <Image 
-                    source={require("../assets/images/logo.png")}
-                    style={styles.logo}
-                    resizeMode='contain'
-                />
-                <Text style={styles.titulo}>TaskFlow</Text>
+                <View style={styles.card}>
+                    <Image
+                        source={require("../assets/images/logo.png")}
+                        style={styles.logo}
+                        resizeMode='contain'
+                    />
+                    <Text style={styles.titulo}>TaskFlow</Text>
 
-                <Text style={styles.descricao}>
-                    Organize sua tarefas de forma simples
-                </Text>
+                    <Text style={styles.descricao}>
+                        Organize sua tarefas de forma simples
+                    </Text>
+
+                    <Pressable
+                        onPress={iniciarAplicacao}
+                        style={({ pressed }) => [styles.botao,
+                        pressed && styles.botaoPressionado
+                        ]}
+                    >
+
+                        {({ pressed }) => (
+                            <Text style={styles.textoBotao}>
+                                {pressed ? "Carregando" : "Iniciar"}
+                            </Text>
+                        )}
+
+                    </Pressable>
+                </View>
             </View>
         </SafeAreaView>
     );
